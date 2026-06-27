f8c026b1a56a273fe7aac4d9cc83791ab5b5baec
victor-430 <oluwatobivictor430@gmail.com>
2026-06-27 22:30:45 +0100

    add cors and helmet

---
+++ config/cors.js
@@ -0,0 +1,21 @@
+import cors from "cors";
+
+const allowedOrigins =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+
+
+export const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      new Error(`CORS blocked ${origin} not allowed`);
+    }
+  },
+
+  methods: ["GET", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  // credentials:true
+};
--- index.js
+++ index.js
@@ -8,14 +8,20 @@ import { routeNotFound } from "./middleware/routeNotFound.js";
 import { connectDb } from "./config/db.js";
 import { requestLogger } from "./middleware/requestLogger.js";
 import { globalRateLimit } from "./middleware/rateLimiter.js";
+import helmet from "helmet";
+import { corsOptions } from "./config/cors.js";
 
 await connectDb();
 
 const app = express();
 
-app.use(globalRateLimit)
+app.use(helmet());
 
-app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use(globalRateLimit);
+
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false }));
 
 // request logger
